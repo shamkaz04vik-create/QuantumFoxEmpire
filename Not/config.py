89f0d0474b6487c449e4ab67196723d9e52ae634
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-05 00:05:17 +0300

    Create config.py

---
+++ Not/config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = "7340839720:AAFMiGuu_P5Va9QylY64ElbMMynPjhTui6M"
+ADMIN_ID = 7209803923
+
+# AI
+OPENROUTER_API_KEY = "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"
+OPENROUTER_MODEL = "openai/gpt-4o-mini"
+
+# Webhook для Render
+WEBHOOK_URL = "https://<your-render-subdomain>.onrender.com/webhook"
+
+# DB
+DATABASE_URL = "postgresql://user:password@host:5432/dbname"
+
+# CryptoBot
+CRYPTOBOT_TOKEN = "<TOKEN из @CryptoBot>"
