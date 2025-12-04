958333590d2f2a9c4c1d8d25bf9254101576723f
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-04 20:06:20 +0300

    Implement basic echo bot using aiogram

---
+++ Not/Bot.py
@@ -0,0 +1,10 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.reply("Quantum Fox Empire работает!")
