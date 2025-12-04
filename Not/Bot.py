3495a7f5e4819381ac82c9d554e3d35e135e6d57
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-05 00:31:30 +0300

    Update Bot.py

--- Not/Bot.py
+++ Not/Bot.py
@@ -1,29 +1,29 @@
 # bot.py
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+
 from config import BOT_TOKEN
-import logging
-from db import init_db
 from handlers import router as user_router
 from admin import router as admin_router
+from db import init_db, ensure_default_partners
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-async def main():
-    # Инициализация БД
+async def start_bot():
+    # init DB
     await init_db()
+    await ensure_default_partners()
 
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
-
-    # Подключаем роутеры
     dp.include_router(user_router)
     dp.include_router(admin_router)
 
-    logger.info("Запуск бота...")
+    logger.info("Starting polling (useful for local testing)...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start_bot())
