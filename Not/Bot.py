e54886b4279db86e3b44c5fa617812ba29259fa3
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-04 21:36:29 +0300

    Update Bot.py

--- Not/Bot.py
+++ Not/Bot.py
@@ -1,11 +1,23 @@
 import asyncio
 import os
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
 
 TOKEN = os.getenv("BOT_TOKEN")
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.reply("Бот работает!")
+
 @dp.message()
 async def echo(message: types.Message):
-    await message.reply("Quantum Fox Empire работает!")
+    await message.reply(message.text)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
