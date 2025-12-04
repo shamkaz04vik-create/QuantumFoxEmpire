f5b8056773b99873be3668300f3599b5e407f633
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-05 02:24:16 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN") or "ТВОЙ_ТОКЕН"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -10,6 +10,3 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.answer("Бот работает!")
-
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
