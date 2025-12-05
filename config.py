9c630361427021454eab209cbccd7dec516d9777
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-05 04:20:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 BOT_TOKEN = "8456865406:AAGqqDLt4PpMf5QrDEPr7dDXymtTb_eN1_o"
-
 ADMIN_ID = 7209803923
 
-WEBHOOK_URL = "https://quantumfoxempire.onrender.com/"
+# OpenRouter
+OPENROUTER_API_KEY = "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"
+OPENROUTER_MODEL = "openai/gpt-4o-mini"
 
+# DB path
 DB_PATH = "database.db"
 
-# --- OpenRouter API ---
-OPENROUTER_API_KEY = "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"
-OPENROUTER_MODEL = "gpt-4o-mini"
+# Webhook
+WEBHOOK_HOST = "https://quantumfoxempire.onrender.com"
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = WEBHOOK_HOST + WEBHOOK_PATH
+
+# Этот параметр нужен только чтобы не ломать импорт
+VPN_PARTNERS = []
