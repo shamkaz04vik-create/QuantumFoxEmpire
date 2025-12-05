3ed923ee7deb3adfa916c4e369141b25c1fa8586
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-05 04:16:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ BOT_TOKEN = "8456865406:AAGqqDLt4PpMf5QrDEPr7dDXymtTb_eN1_o"
 
 ADMIN_ID = 7209803923
 
+WEBHOOK_URL = "https://quantumfoxempire.onrender.com/"
+
 DB_PATH = "database.db"
 
-VPN_PARTNERS = {
-    "molniya": "https://molniyavpn.com/?ref={user}",
-    "kovalenko": "https://kovavpn.com/?ref={user}",
-}
-
-WEBHOOK_URL = "https://quantumfoxempire.onrender.com/"
+# --- OpenRouter API ---
+OPENROUTER_API_KEY = "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"
+OPENROUTER_MODEL = "gpt-4o-mini"
