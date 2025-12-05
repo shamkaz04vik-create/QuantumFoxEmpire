be417a55adb61c377bc03997b004e9ab32a23885
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-05 03:24:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,17 @@ OPENROUTER_API_KEY = "sk-or-v1-76ce009e36b30c65d042ae400160de3e1a2cf8d99be440035
 OPENROUTER_MODEL = "openai/gpt-4o-mini"
 
 # --- Deployment ---
-WEBHOOK_URL = "https://quantumfoxempire.onrender.com/webhook"
+WEBHOOK_URL = "https://quantumfoxempire.onrender.com/"
 
 # --- Database ---
-DB_PATH = "database.db"   # локальная БД (работает на Render)
+DB_PATH = "database.db"      # <-- ВАЖНО: больше НЕ /data/
+
+# --- Payments ---
+CRYPTOBOT_TOKEN = ""
+MANUAL_WALLET = "TXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# --- VPN Partners ---
+VPN_PARTNERS = {
+    "molniya": "https://t.me/molniya_vpn_bot?start=john0_8_{user}",
+    "kovalenko": "https://t.me/Kovalenkovpn_bot?start=john0_8_{user}"
+}
