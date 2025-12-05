829bb20e6a9c80c792380bea98e7220c4947bb66
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-05 04:12:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,12 @@
-# ===============================
-#   CONFIG.PY — полный файл
-# ===============================
-
-# --- Telegram Bot Token ---
 BOT_TOKEN = "8456865406:AAGqqDLt4PpMf5QrDEPr7dDXymtTb_eN1_o"
 
-# --- Админ ID ---
 ADMIN_ID = 7209803923
 
-# --- Путь к базе данных ---
 DB_PATH = "database.db"
 
-# --- VPN партнёры ---
 VPN_PARTNERS = {
     "molniya": "https://molniyavpn.com/?ref={user}",
     "kovalenko": "https://kovavpn.com/?ref={user}",
 }
 
-# --- Render Webhook URL ---
 WEBHOOK_URL = "https://quantumfoxempire.onrender.com/"
